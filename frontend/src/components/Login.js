05426071111d69a3fce01859966cb0dace79c0c9
NagaSaranyaK <naga.saranya274@gmail.com>
2026-03-06 22:04:27 +0530

    Connect frontend to render backend

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -20,7 +20,7 @@ const Login = () => {
     setError('');
 
     try {
-      const response = await fetch('http://localhost:5000/login', {
+      const response = await fetch('https://login-backend-luuv.onrender.com', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password }),
